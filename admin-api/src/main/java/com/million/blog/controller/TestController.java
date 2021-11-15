478fd14924af4bf3d9945115b67bb657474765b3
‘wujunjie’ <1658634188@qq.com>
2021-11-16 01:05:06 +0800

    登录拦截器

--- admin-api/src/main/java/com/million/blog/controller/TestController.java
+++
@@ -1,8 +0,0 @@
-package com.million.blog.controller;
-
-/**
- * @Author: studyboy
- * @Date: 2021/11/16  0:12
- */
-public class TestController {
-}
---
+++ admin-api/src/main/java/com/million/blog/handler/LoginInterceptor.java
@@ -0,0 +1,6 @@
+package com.million.blog.handler;/**
+ *  @Author: studyboy
+ *  @Date: 2021/11/16  1:04
+ *
+ */public class LoginInterceptor {
+}
